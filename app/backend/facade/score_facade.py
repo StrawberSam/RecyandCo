32affb063d32aa1760098b0a22662dd9d43d1bae
StrawberSam <s.roche53@hotmail.com>
2025-10-06 16:17:19 +0200

    🔨 | Changement de nom des routes pour correspondre avec score

--- app/backend/facade/score_facade.py
+++ app/backend/facade/score_facade.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, request, current_app
 
-auth_bp = Blueprint("auth", __name__)
+score_bp = Blueprint("score", __name__)
 
-@auth_bp.route("/api/scores", methods=["POST"])
+@score_bp.route("/api/scores", methods=["POST"])
 def add_scores():
     score_service = current_app.config["services"]["score"]
     data = request.get_json()
@@ -12,7 +12,7 @@ def add_scores():
         )
     return jsonify(response), response["status_code"]
 
-@auth_bp.route("/api/scores/me", methods=["GET"])
+@score_bp.route("/api/scores/me", methods=["GET"])
 def user_score():
     auth_service = current_app.config["services"]["auth"]
     score_service = current_app.config["services"]["score"]
@@ -30,7 +30,7 @@ def user_score():
     response = score_service.get_user_scores(user_id)
     return jsonify(response), response["status_code"]
 
-@auth_bp.route("/api/leaderboard", methods=["GET"])
+@score_bp.route("/api/leaderboard", methods=["GET"])
 def leaderboard():
     score_service = current_app.config["services"]["score"]
     limit = request.args.get("limit", default=15, type=int)
