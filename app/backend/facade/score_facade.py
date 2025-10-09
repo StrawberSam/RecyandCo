78ad029d1f3ac8dd46b2c721d7ac23c6aa52bd5c
StrawberSam <s.roche53@hotmail.com>
2025-10-09 10:43:41 +0200

    🐛 | Ajout de la récupération du user_id par le token

--- app/backend/facade/score_facade.py
+++ app/backend/facade/score_facade.py
@@ -4,10 +4,23 @@ score_bp = Blueprint("score", __name__)
 
 @score_bp.route("/api/scores", methods=["POST"])
 def add_scores():
+    auth_service = current_app.config["services"]["auth"]
     score_service = current_app.config["services"]["score"]
+
+    auth_header = request.headers.get("Authorization")
+    if not auth_header:
+        return jsonify({"success": False, "message": "Token manquant"}), 401
+
+    token = auth_header.split(" ")[1]
+    user = auth_service.get_user_by_id(token)
+    if not user.get("success"):
+        return jsonify(user), 401
+
+    user_id = user["data"]["id"]
+
     data = request.get_json()
     response = score_service.add_score(
-        user_id=data.get("user_id"),
+        user_id=user_id,
         points=data.get("points")
         )
     return jsonify(response), response["status_code"]
