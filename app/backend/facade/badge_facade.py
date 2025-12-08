e2cd613945d321b27acb2c944ad00778d35971dc
StrawberSam <s.roche53@hotmail.com>
2025-12-08 13:19:43 +0100

    ♻️ refactor(badge_facade): utilisation auth_utils (DRY)
    - Remplacement vérification token manuelle par verify_token_and_get_user_id()
    - Réduction du code de vérification d'authentification
    - 1 route protégée refactorisée (/api/badges/me)

--- app/backend/facade/badge_facade.py
+++ app/backend/facade/badge_facade.py
@@ -1,22 +1,15 @@
 from flask import Blueprint, jsonify, request, current_app
-
+from utils.auth_utils import verify_token_and_get_user_id
 badge_bp = Blueprint("badge", __name__)
 
 @badge_bp.route("/api/badges/me", methods=["GET"])
 def user_badges():
-    auth_service = current_app.config["services"]["auth"]
+        # Vérification token et récupération user_id
+    user_id, error = verify_token_and_get_user_id()
+    if error:
+        return jsonify(error), error["status_code"]
+
     badge_service = current_app.config["services"]["badge"]
-
-    token = request.cookies.get("access_token")
-    if not token:
-        return jsonify({"success": False, "message": "Token manquant"}), 401
-
-    user = auth_service.get_user_by_id(token) # Récupère id par le token
-
-    if not user.get("success"): # si problème avec le token
-        return jsonify(user), 401
-
-    user_id = user["data"]["id"]
     response = badge_service.get_user_badges(user_id)
     return jsonify(response), response["status_code"]
 
